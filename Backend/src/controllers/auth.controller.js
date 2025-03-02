88076a2517d4b7a63d8ad7c7d6fffa7188f5d14a
Hoàng Văn Chuẩn <hoangchuanks@gmail.com>
2025-03-02 23:33:41 +0700

    check postman

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,39 @@
-export const signup = (req,rex) => {
-    res.send("Signup route");
+import User from "../models/user.model.js"
+import bcrypt from "bcrypt"
+export const signup = async (req,rex) => {
+    const {fullName ,email,password} = req.body;
+    try{
+        if(password.length < 6){
+        return res.status(400).json({message:"pass word must be 6"})
+    }
+    const user = await User.findOne({email})
+    if (user) return res.status(400).json({message:"email already"})
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    
+    const newUser = new User({
+        fullName,
+        email,
+        password: hashedPassword,
+      });
+      if (newUser) {
+        // generate jwt token here
+        generateToken(newUser._id, res);
+        await newUser.save();
+  
+        res.status(201).json({
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+          profilePic: newUser.profilePic,
+        });
+      } else {
+        res.status(400).json({ message: "Invalid user data" });
+      }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
 export const login = (req,rex) => {
