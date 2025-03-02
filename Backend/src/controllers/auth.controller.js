e2595e1dc265883573a773f10442d7adbada2dad
Hoàng Văn Chuẩn <hoangchuanks@gmail.com>
2025-03-02 11:49:45 +0700

    run1

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req,rex) => {
+    res.send("Signup route");
+};
+
+export const login = (req,rex) => {
+    res.send("Signup route");
+};
+
+export const logout = (req,rex) => {
+    res.send("Signup route");
+};
